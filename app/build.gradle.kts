48cce3b597243dde475cfb1e94110f70b1db9aa9
Eliezer Sant Ana <er.7santana@gmail.com>
2024-10-31 21:00:11 -0300

    atualizando versoes de dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
     namespace = "br.com.shaft.aluvery"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "br.com.shaft.aluvery"
--- app/src/main/java/br/com/shaft/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/shaft/aluvery/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Greeting("Eliezer", Modifier.padding(innerPadding))
                 }
             }
         }
@@ -32,16 +32,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Text("Eliezer", modifier = modifier)
+        Button(onClick = { /* Do something! */ }) {
+            Text("Tap me")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true, device = "id:pixel_5")
 @Composable
 fun GreetingPreview() {
     AluveryTheme {
-        Greeting("Android")
+        Greeting("Eliezer")
     }
 }
