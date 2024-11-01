bad7627bfacad959893a88e0325248d58d408290
Eliezer Sant Ana <er.7santana@gmail.com>
2024-11-01 01:35:49 -0300

    adicionando estrutura de produtos

--- app/src/main/java/br/com/shaft/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/shaft/aluvery/MainActivity.kt
@@ -45,10 +45,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import br.com.shaft.aluvery.extensions.toBrazilianCurrency
+import br.com.shaft.aluvery.models.Product
 import br.com.shaft.aluvery.ui.theme.AluveryTheme
 import br.com.shaft.aluvery.ui.theme.Purple40
 import br.com.shaft.aluvery.ui.theme.Purple80
 import br.com.shaft.aluvery.ui.theme.Teal200
+import java.math.BigDecimal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,7 +60,58 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    ProductsSection()
+                    ProductsSection(listOf(
+                        Product(
+                            "Product 1",
+                            BigDecimal(14.99),
+                            R.drawable.ic_launcher_background
+                        ),
+                        Product(
+                            "Product 2",
+                            BigDecimal(19.99),
+                            R.drawable.ic_launcher_background
+                        ),
+                        Product(
+                            "Product 3",
+                            BigDecimal(24.99),
+                            R.drawable.ic_launcher_background
+                        ),
+                        Product(
+                            "Product 4",
+                            BigDecimal(29.99),
+                            R.drawable.ic_launcher_background
+                        ),
+                        Product(
+                            "Product 5",
+                            BigDecimal(34.99),
+                            R.drawable.ic_launcher_background
+                        ),
+                        Product(
+                            "Product 6",
+                            BigDecimal(39.99),
+                            R.drawable.ic_launcher_background
+                        ),
+                        Product(
+                            "Product 7",
+                            BigDecimal(44.99),
+                            R.drawable.ic_launcher_background
+                        ),
+                        Product(
+                            "Product 8",
+                            BigDecimal(49.99),
+                            R.drawable.ic_launcher_background
+                        ),
+                        Product(
+                            "Product 9",
+                            BigDecimal(54.99),
+                            R.drawable.ic_launcher_background
+                        ),
+                        Product(
+                            "Product 10",
+                            BigDecimal(59.99),
+                            R.drawable.ic_launcher_background
+                        ),
+                    ))
                 }
             }
 
@@ -66,7 +120,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ProductsSection() {
+fun ProductsSection(products: List<Product>) {
     Column {
         Text(
             "Promoções",
@@ -91,15 +145,15 @@ fun ProductsSection() {
             ,
             horizontalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            ProductItem()
-            ProductItem()
-            ProductItem()
+            products.forEach { product ->
+                ProductItem(product)
+            }
         }
     }
 }
 
 @Composable
-fun ProductItem() {
+fun ProductItem(product: Product) {
     Surface(
         shape = RoundedCornerShape(15.dp),
         shadowElevation = 4.dp
@@ -119,28 +173,29 @@ fun ProductItem() {
 
             ) {
                 Image(
-                    painter = painterResource(R.drawable.ic_launcher_background),
+                    painter = painterResource(product.image),
                     contentDescription = null,
                     Modifier
                         .size(imageSize)
                         .offset(y = (imageSize / 2))
                         .clip(shape = CircleShape)
                         .align(BottomCenter)
-                        .border(2.dp, Color.White, shape = CircleShape)
+                        .border(2.dp, Color.White, shape = CircleShape),
+                    contentScale = androidx.compose.ui.layout.ContentScale.Crop
                 )
             }
             Spacer(Modifier.height(imageSize/2))
             Column(Modifier.fillMaxSize()) {
                 Column(Modifier.padding(16.dp)) {
                     Text(
-                        LoremIpsum(50).values.first(),
+                        product.name,
                         fontSize = 18.sp,
                         fontWeight = FontWeight(700),
                         maxLines = 2,
                         overflow = TextOverflow.Ellipsis
                     )
                     Text(
-                        "R$ 14,99",
+                        product.price.toBrazilianCurrency(),
                         Modifier.padding(top = 8.dp),
                         fontSize = 14.sp,
                         fontWeight = FontWeight(400),
@@ -155,7 +210,13 @@ fun ProductItem() {
 @Composable
 private fun ProductItemPreview() {
     AluveryTheme {
-        ProductItem()
+        ProductItem(
+            product = Product(
+                LoremIpsum(50).values.first(),
+                BigDecimal(14.99),
+                R.drawable.ic_launcher_background
+            )
+        )
     }
 }
 
@@ -163,6 +224,22 @@ private fun ProductItemPreview() {
 @Composable
 private fun ProductsSectionPreview() {
     AluveryTheme {
-        ProductsSection()
+        ProductsSection(listOf(
+            Product(
+                "Hamburguer",
+                BigDecimal(14.99),
+                R.drawable.burger
+            ),
+            Product(
+                "Batata frita",
+                BigDecimal(19.99),
+                R.drawable.fries
+            ),
+            Product(
+                "Pizza",
+                BigDecimal(24.99),
+                R.drawable.pizza
+            ),
+        ))
     }
 }
