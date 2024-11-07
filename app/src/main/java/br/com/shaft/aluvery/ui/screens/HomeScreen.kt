3de9a20a6a5aab4956028739181b82e019340a02
Eliezer Sant Ana <er.7santana@gmail.com>
2024-11-06 22:25:54 -0300

    adicionando State Holder da Home screen

--- app/src/main/java/br/com/shaft/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/shaft/aluvery/ui/screens/HomeScreen.kt
@@ -1,33 +1,22 @@
 package br.com.shaft.aluvery.ui.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import br.com.shaft.aluvery.R
 import br.com.shaft.aluvery.models.Product
 import br.com.shaft.aluvery.sampledata.sampleProducts
 import br.com.shaft.aluvery.sampledata.sampleSections
@@ -38,13 +27,11 @@ import br.com.shaft.aluvery.ui.theme.AluveryTheme
 
 private const val TAG = "HomeScreen"
 
-@Composable
-fun HomeScreen(
-    sections: Map<String, List<Product>>,
-    searchText: String = ""
-) {
-    var text by remember { mutableStateOf(searchText) }
-    val filteredProducts = remember(text) {
+class HomeScreenUiState(searchText: String = "") {
+
+    var text by mutableStateOf(searchText)
+
+    val filteredProducts get() =
         if (text.isNotBlank()) {
             sampleProducts.filter { product ->
                 product.name.contains(text, ignoreCase = true) || product.description?.contains(
@@ -52,12 +39,22 @@ fun HomeScreen(
                 ) ?: false
             }
         } else emptyList()
-    }
 
+    fun isShowSections(): Boolean {
+        return text.isBlank()
+    }
+}
+
+@Composable
+fun HomeScreen(
+    sections: Map<String, List<Product>>,
+    searchText: String = ""
+) {
+    val state = remember { HomeScreenUiState(searchText) }
     Column {
 
-        SearchTextField(text) {
-            text = it
+        SearchTextField(state.text) {
+            state.text = it
         }
 
         LazyColumn(
@@ -66,7 +63,7 @@ fun HomeScreen(
                 .padding(vertical = 16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            if (text.isBlank()) {
+            if (state.isShowSections()) {
                 for (section in sections) {
                     val title = section.key
                     val products = section.value
@@ -75,7 +72,7 @@ fun HomeScreen(
                     }
                 }
             } else {
-                items(filteredProducts) { product ->
+                items(state.filteredProducts) { product ->
                     CardProductItem(product, Modifier.padding(horizontal = 16.dp))
                 }
             }
