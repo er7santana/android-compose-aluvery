011f64de9079a148288f3a1f2f903f3c3d156c23
Eliezer Sant Ana <er.7santana@gmail.com>
2024-11-08 00:14:43 -0300

    aplicando o stateFlow no ViewModel

--- app/src/main/java/br/com/shaft/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/shaft/aluvery/ui/screens/HomeScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,7 +25,7 @@ import br.com.shaft.aluvery.ui.viewmodels.HomeScreenViewModel
 
 @Composable
 fun HomeScreen(viewModel: HomeScreenViewModel) {
-    val state = viewModel.uiState
+    val state by viewModel.uiState.collectAsState()
     HomeScreen(state)
 }
 
--- app/src/main/java/br/com/shaft/aluvery/ui/viewmodels/HomeScreenViewModel.kt
+++ app/src/main/java/br/com/shaft/aluvery/ui/viewmodels/HomeScreenViewModel.kt
@@ -1,8 +1,5 @@
 package br.com.shaft.aluvery.ui.viewmodels
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.shaft.aluvery.dao.ProductDao
@@ -11,20 +8,32 @@ import br.com.shaft.aluvery.sampledata.sampleCandies
 import br.com.shaft.aluvery.sampledata.sampleDrinks
 import br.com.shaft.aluvery.sampledata.sampleProducts
 import br.com.shaft.aluvery.ui.states.HomeScreenUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class HomeScreenViewModel: ViewModel() {
 
     private val dao = ProductDao()
 
-    var uiState: HomeScreenUiState by mutableStateOf(HomeScreenUiState(
-        onSearchChange = {
-            uiState = uiState.copy(searchText = it, filteredProducts = filteredProducts(it))
-        }
-    ))
-        private set
+    private var _uiState: MutableStateFlow<HomeScreenUiState> = MutableStateFlow(
+        HomeScreenUiState(
+
+        ))
+    val uiState get() = _uiState.asStateFlow()
+
 
     init {
+        _uiState.update { currentState ->
+            currentState.copy(onSearchChange = {
+                _uiState.value = _uiState.value.copy (
+                    searchText = it,
+                    filteredProducts = filteredProducts(it)
+                )
+            })
+        }
+
         viewModelScope.launch {
             dao.products().collect { products ->
                 val sections = mapOf(
@@ -33,7 +42,7 @@ class HomeScreenViewModel: ViewModel() {
                     "Doces" to sampleDrinks,
                     "Bebidas" to sampleCandies
                 )
-                uiState = uiState.copy(sections = sections, filteredProducts = filteredProducts(uiState.searchText))
+                _uiState.value = _uiState.value.copy(sections = sections, filteredProducts = filteredProducts(_uiState.value.searchText))
             }
         }
     }
@@ -46,8 +55,8 @@ class HomeScreenViewModel: ViewModel() {
 
     private fun filteredProducts(searchText: String): List<Product> {
         return if (searchText.isNotBlank()) {
-            sampleProducts.filter(containsInNameOrDescription(uiState.searchText)) +
-                    dao.products().value.filter(containsInNameOrDescription(uiState.searchText))
+            sampleProducts.filter(containsInNameOrDescription(_uiState.value.searchText)) +
+                    dao.products().value.filter(containsInNameOrDescription(_uiState.value.searchText))
         } else emptyList()
     }
 }
